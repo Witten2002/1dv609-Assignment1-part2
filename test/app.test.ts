24b8cd7cf6373831a7575fad6e434c9764a901e2
Ludwig Wittenberg <lw223cq@student.lnu.se>
2024-11-20 17:15:22 +0100

    Testing TDD RED

--- test/app.test.ts
+++ test/app.test.ts
@@ -3,11 +3,16 @@ import { add } from '../src/app'
 describe('add function', () => {
   test('Test Add Method', () => {
     const result = add(1, 0)
-      expect(result).toBe(1)
+    expect(result).toBe(1)
   })
 
   test('Test Add Method', () => {
     const result = add(2, 0)
-      expect(result).toBe(2)
+    expect(result).toBe(2)
+  })
+
+  test('Test Add Method', () => {
+    const result = add(1, 2)
+    expect(result).toBe(3)
   })
 })
