0c6c9396e81dd250f2186d3d7cd8c0b70f9267ea
Ludwig Wittenberg <lw223cq@student.lnu.se>
2024-11-20 18:39:57 +0100

    Paper Beats Rock RED

--- test/HandGuesture/Paper.test.ts
+++ test/HandGuesture/Paper.test.ts
@@ -1,5 +1,6 @@
 import { Paper } from '../../src/model/HandGesture/Paper'
 import { Scissor } from '../../src/model/HandGesture/Scissor'
+import { Rock } from '../../src/model/HandGesture/Rock'
 
 describe('Paper class under test', () => {
   const getSUT = () => {
@@ -10,10 +11,22 @@ describe('Paper class under test', () => {
     return new Scissor()
   }
 
+  const getRock = () => {
+    return new Rock()
+  }
+
   test('Paper Beats Scissors', () => {
     const sut = getSUT()
     const actual = sut.beats(getScissor())
     const expected = false
+
+    expect(actual).toBe(expected)
+  })
+
+  test('Paper Beats Rock', () => {
+    const sut = getSUT()
+    const actual = sut.beats(getRock())
+    const expected = true
 
     expect(actual).toBe(expected)
   })
