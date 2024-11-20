ede282ebd497b2d7e266390a26272882e1a3a687
Ludwig Wittenberg <lw223cq@student.lnu.se>
2024-11-20 17:12:49 +0100

    Testing TDD RED

--- test/app.test.ts
+++ test/app.test.ts
@@ -5,4 +5,9 @@ describe('add function', () => {
     const result = add(1, 0)
       expect(result).toBe(1)
   })
+
+  test('Test Add Method', () => {
+    const result = add(2, 0)
+      expect(result).toBe(2)
+  })
 })
