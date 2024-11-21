8fc9dfc0fd6f580ee6ea1508c85028d577ad723d
Ludwig Wittenberg <lw223cq@student.lnu.se>
2024-11-21 18:48:33 +0100

    Rules Player Win PaperVsRock GREEN

--- src/model/Rules.ts
+++ src/model/Rules.ts
@@ -2,10 +2,11 @@ import { Player } from './Player.ts'
 import { ComputerPlayer } from './ComputerPlayer.ts'
 import { GameResult } from './enums/GameResult.ts'
 import { Rock } from './HandGesture/Rock.ts'
+import { Paper } from './HandGesture/Paper.ts'
 
 class Rules {
   deternimateWinner(player: Player, computerPlayer: ComputerPlayer) {
-    if (player.getHandGuesture() instanceof Rock) {
+    if (player.getHandGuesture() instanceof Rock || player.getHandGuesture() instanceof Paper) {
       return GameResult.PLAYER
     }
 
