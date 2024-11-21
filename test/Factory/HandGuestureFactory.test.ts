999d9e03a08cb6a85e6f7fd50b2abc90608d444d
Ludwig Wittenberg <lw223cq@student.lnu.se>
2024-11-21 13:55:16 +0100

    HandGestureFactory With Paper RED

--- test/Factory/HandGuestureFactory.test.ts
+++ test/Factory/HandGuestureFactory.test.ts
@@ -1,6 +1,7 @@
 import { HandGestureFactory } from '../../src/model/Factory/HandGuestureFactory'
 import { UserChoice } from '../../src/model/enums/UserChoice'
 import { Rock } from '../../src/model/HandGesture/Rock'
+import { Paper } from '../../src/model/HandGesture/Paper'
 
 describe('HandGestureFactory Under Test', () => {
   test('HandGestureFactory With Rock', () => {
@@ -8,6 +9,15 @@ describe('HandGestureFactory Under Test', () => {
     const actual = sut.createHandGuesture(UserChoice.ROCK)
 
     const expected = new Rock()
+
+    expect(actual).toStrictEqual(expected)
+  })
+
+  test('HandGestureFactory With Paper', () => {
+    const sut = new HandGestureFactory()
+    const actual = sut.createHandGuesture(UserChoice.PAPER)
+
+    const expected = new Paper()
 
     expect(actual).toStrictEqual(expected)
   })
