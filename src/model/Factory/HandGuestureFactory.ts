53cdde28f62af710f9e53b6b81cd3f3bf97222bd
Ludwig Wittenberg <lw223cq@student.lnu.se>
2024-11-21 13:53:58 +0100

    HandGestureFactory With Rock GREEN

--- src/model/Factory/HandGuestureFactory.ts
+++ src/model/Factory/HandGuestureFactory.ts
@@ -1,9 +1,10 @@
 import { UserChoice } from '../enums/UserChoice.ts'
 import { HandGuesture } from '../HandGesture/HandGesture.ts'
+import { Rock } from '../HandGesture/Rock.ts'
 
 class HandGestureFactory {
   createHandGuesture(userChoice: UserChoice): HandGuesture {
-    return
+    return new Rock()
   }
 }
 
