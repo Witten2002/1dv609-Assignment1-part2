e16590535886ee0259b8e2a376810fc0da4f6ba2
Ludwig Wittenberg <lw223cq@student.lnu.se>
2024-11-21 13:24:08 +0100

    Player HandGuesture Paper GREEN

--- src/model/Player.ts
+++ src/model/Player.ts
@@ -1,11 +1,16 @@
 import { HandGuesture } from './HandGesture/HandGesture.ts'
 import { Rock } from './HandGesture/Rock.ts'
+import { Paper } from './HandGesture/Paper.ts'
 
 class Player {
   #handGuesutre: HandGuesture
 
   setHandGuesture(handGuesture: HandGuesture) {
-    this.#handGuesutre = new Rock()
+    if (handGuesture instanceof Rock) {
+      this.#handGuesutre = new Rock()
+    } else {
+      this.#handGuesutre = new Paper()
+    }
   }
 
   getHandGuesture(): HandGuesture {
