11f7b4c49182558e3ac0d564a564768646083a18
Ludwig Wittenberg <lw223cq@student.lnu.se>
2024-11-21 13:57:34 +0100

    HandGestureFactory With Scissor RED

--- test/Factory/HandGuestureFactory.test.ts
+++ test/Factory/HandGuestureFactory.test.ts
@@ -2,6 +2,7 @@ import { HandGestureFactory } from '../../src/model/Factory/HandGuestureFactory'
 import { UserChoice } from '../../src/model/enums/UserChoice'
 import { Rock } from '../../src/model/HandGesture/Rock'
 import { Paper } from '../../src/model/HandGesture/Paper'
+import { Scissor } from '../../src/model/HandGesture/Scissor'
 
 describe('HandGestureFactory Under Test', () => {
   test('HandGestureFactory With Rock', () => {
@@ -18,6 +19,15 @@ describe('HandGestureFactory Under Test', () => {
     const actual = sut.createHandGuesture(UserChoice.PAPER)
 
     const expected = new Paper()
+
+    expect(actual).toStrictEqual(expected)
+  })
+
+  test('HandGestureFactory With Scissor', () => {
+    const sut = new HandGestureFactory()
+    const actual = sut.createHandGuesture(UserChoice.SCISSOR)
+
+    const expected = new Scissor()
 
     expect(actual).toStrictEqual(expected)
   })
