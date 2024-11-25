b18c59efb5d156cb471985b1ebf8f9139cbb25fc
Ludwig Wittenberg <lw223cq@student.lnu.se>
2024-11-25 12:31:52 +0100

    Added mock to Rock.test

--- test/HandGuesture/Rock.test.ts
+++ test/HandGuesture/Rock.test.ts
@@ -1,8 +1,36 @@
 import { Rock } from '../../src/model/HandGesture/Rock.js'
 import { Paper } from '../../src/model/HandGesture/Paper.js'
 import { Scissor } from '../../src/model/HandGesture/Scissor.js'
+import { UserChoice } from '../../src/model/enums/UserChoice.js'
+
+jest.mock('../../src/model/HandGesture/Scissor', () => {
+  return {
+    Scissor: jest.fn().mockImplementation(function () {
+      this.type = UserChoice.SCISSOR
+    })
+  }
+})
+
+jest.mock('../../src/model/HandGesture/Paper', () => {
+  return {
+    Paper: jest.fn().mockImplementation(function () {
+      this.type = UserChoice.PAPER
+    })
+  }
+})
 
 describe('Rock Class Under Test', () => {
+  let sut: Rock
+  let scissor: Scissor
+  let paper: Paper
+
+  
+  beforeEach(() => {
+    sut = new Rock()
+    scissor = new Scissor()
+    paper = new Paper()
+  })
+
   const setUpTest = (param) => {
     const sut = new Rock()
     const actual = sut.beats(param)
